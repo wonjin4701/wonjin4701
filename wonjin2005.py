fdd02fbc09c5fbb535c81ed7fe81d26626bddae3
wonjin4701 <62679808+wonjin4701@users.noreply.github.com>
2020-06-01 16:31:41 +0900

    Update wonjin2005.py

--- wonjin2005.py
+++ wonjin2005.py
@@ -10,7 +10,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.online, activity=discord.Game("테스트중"))
+  await client.change_presence(status=discord.Status.online, activity=discord.Game("원진#7917"))
   print("원진#7917") 
   print(client.user.name) 
   print(client.user.id) 
@@ -20,9 +20,9 @@ async def on_message(message):
  
   if message.content.startswith('/인증'): 
     author = message.guild.get_member(int(message.author.id))
-    role = discord.utils.get(message.guild.roles, name="TEST") 
+    role = discord.utils.get(message.guild.roles, name="[V] 커뮤니티 유저") 
     await author.add_roles(role)
-    await message.channel.send('인증완료') 
+    await message.channel.send('V FIVEM Community 인증이 완료되었습니다!') 
 
 
 
